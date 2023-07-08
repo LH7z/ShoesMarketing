bc251926b5c72cc9a768a8e44e7f224a2766af1d
luis henrique <luisham2001@gmail.com>
2023-07-08 16:42:31 +0100

    models ready

---
+++ db/migrate/20230708153150_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.integer :price
+      t.integer :rating
+      t.string :comments
+
+      t.timestamps
+    end
+  end
+end
